77d22e0baa76ce798f003009575e0799abd3ef9d
rsoliz99 <72117825+rsoliz99@users.noreply.github.com>
2025-06-10 19:45:13 -0500

    Add DemoRoutes and updated App.js for /demo routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// App.js – Includes /demo redirect to homepage
+// App.js – Includes sandbox routing support for /demo/* via DemoRoutes
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import EducatorLoginPage from "./pages/EducatorLoginPage";
 import EducatorSignupPage from "./pages/EducatorSignupPage";
@@ -9,23 +9,20 @@ import DistrictLoginPage from "./pages/DistrictLoginPage";
 import DistrictSignupPage from "./pages/DistrictSignupPage";
 import DistrictDashboard from "./pages/DistrictDashboard";
 import NotFoundPage from "./pages/NotFoundPage";
+import DemoRoutes from "./DemoRoutes";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* Redirect /demo to homepage */}
-        <Route path="/demo" element={<Navigate to="/" />} />
+        {/* Sandbox routes */}
+        <Route path="/demo/*" element={<DemoRoutes />} />
 
-        {/* Core routes */}
+        {/* Main site routes */}
         <Route path="/" element={<HomePage />} />
-
-        {/* Educator Routes */}
         <Route path="/educator/login" element={<EducatorLoginPage />} />
         <Route path="/educator/signup" element={<EducatorSignupPage />} />
         <Route path="/educator/dashboard" element={<EducatorDashboard />} />
-
-        {/* District Routes */}
         <Route path="/district/login" element={<DistrictLoginPage />} />
         <Route path="/district/signup" element={<DistrictSignupPage />} />
         <Route path="/district/dashboard" element={<DistrictDashboard />} />
