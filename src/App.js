5b80cad506e13b4496f19f9d440daa8a2d8fc729
rsoliz99 <72117825+rsoliz99@users.noreply.github.com>
2025-06-10 19:15:49 -0500

    Redirect /demo to homepage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import EducatorLoginPage from './pages/EducatorLoginPage';
-import EducatorSignupPage from './pages/EducatorSignupPage';
-import EducatorDashboard from './pages/EducatorDashboard';
-import EducatorRequestPage from './pages/EducatorRequestPage';
-import DistrictLoginPage from './pages/DistrictLoginPage';
-import DistrictSignupPage from './pages/DistrictSignupPage';
-import DistrictDashboard from './pages/DistrictDashboard';
-import EducatorForgotPasswordPage from './pages/EducatorForgotPasswordPage';
-import DistrictForgotPasswordPage from './pages/DistrictForgotPasswordPage';
-
+// App.js – Includes /demo redirect to homepage
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import EducatorLoginPage from "./pages/EducatorLoginPage";
+import EducatorSignupPage from "./pages/EducatorSignupPage";
+import EducatorDashboard from "./pages/EducatorDashboard";
+import DistrictLoginPage from "./pages/DistrictLoginPage";
+import DistrictSignupPage from "./pages/DistrictSignupPage";
+import DistrictDashboard from "./pages/DistrictDashboard";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/educator/login" element={<EducatorLoginPage />} />
-        <Route path="/educator/create" element={<EducatorSignupPage />} />
-	<Route path="/educator/dashboard" element={<EducatorDashboard />} />
-	<Route path="/educator/request" element={<EducatorRequestPage />} />
-        <Route path="/district/login" element={<DistrictLoginPage />} />
-	<Route path="/district/signup" element={<DistrictSignupPage />} />
-        <Route path="/district/dashboard" element={<DistrictDashboard />} />
-	<Route path="/educator/forgot-password" element={<EducatorForgotPasswordPage />} />
-	<Route path="/district/forgot-password" element={<DistrictForgotPasswordPage />} />
+        {/* Redirect /demo to homepage */}
+        <Route path="/demo" element={<Navigate to="/" />} />
 
+        {/* Core routes */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Educator Routes */}
+        <Route path="/educator/login" element={<EducatorLoginPage />} />
+        <Route path="/educator/signup" element={<EducatorSignupPage />} />
+        <Route path="/educator/dashboard" element={<EducatorDashboard />} />
+
+        {/* District Routes */}
+        <Route path="/district/login" element={<DistrictLoginPage />} />
+        <Route path="/district/signup" element={<DistrictSignupPage />} />
+        <Route path="/district/dashboard" element={<DistrictDashboard />} />
+
+        {/* Catch-all */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
